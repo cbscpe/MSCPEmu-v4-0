36abc4e1732c19be4cb2ba54da718b109f48ee17
Peter Schranz <peter@5volts.ch>
2025-12-25 21:14:57 +0100

    new status command

---
+++ CLI-status.asm
@@ -0,0 +1,46 @@
+;--------------------------------------------------------------------------
+;
+;	Show Status
+;
+.macro	bytestatus
+	call	print
+.if (STRLEN(@0)&1)
+.db	@0, 0x00
+.else
+.db	@0, 0x00, 0x00
+.endif
+	lds	r24, @1
+	sts	pprint+0, r24
+	call	print
+	.db	" 0x", 0x80, CR, LF, 0, 0
+.endmacro
+
+.macro	wordstatus
+	call	print
+.if (STRLEN(@0)&1)
+.db	@0, 0x00
+.else
+.db	@0, 0x00, 0x00
+.endif
+	lds	r24, @1+0
+	sts	pprint+0, r24
+	lds	r24, @1+1
+	sts	pprint+1, r24
+	call	print
+	.db	" 0x", 0x81, 0x80, CR, LF, 0
+.endmacro
+
+
+printstatus:
+	call	seroutcrlf
+	bytestatus	"nguard        ", nguard
+	wordstatus	"log_pointer   ", log_pointer
+#ifdef testout
+	wordstatus	"tesoutptr     ", tesoutptr
+#endif
+	wordstatus	"heap          ", heap
+	wordstatus	"              ", heap+2
+	clc
+	ret
+
+
