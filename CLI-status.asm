b7e72586ff4fb2dc5e009c08ace606605bbf0b19
Peter Schranz <peter@5volts.ch>
2026-02-16 09:26:02 +0100

    Print SysUpTime as ddd hh:mm:ss

--- CLI-status.asm
+++ CLI-status.asm
@@ -59,6 +59,67 @@ printstatus:
 	call	print
 	.db	"SysUpTime ......................:", 0xd0, "s", CR, LF, 0
 
+;
+;	Calculate DAYs
+;
+	clr	r24
+	clr	r25
+printstatus010:
+	subi	r16, byte1(86400)
+	sbci	r17, byte2(86400)
+	sbci	r18, byte3(86400)
+	sbci	r19, byte4(86400)
+	brmi	printstatus020
+	adiw	r25:r24, 1
+	rjmp	printstatus010
+
+printstatus020:
+	sts	pprint+0, r24		; Store DAYs
+	sts	pprint+1, r25
+	subi	r16, byte1(-86400)	; Compensate surplus subtract
+	sbci	r17, byte2(-86400)
+	sbci	r18, byte3(-86400)
+	sbci	r19, byte4(-86400)
+;
+;	Calculate HOURs
+;
+	clr	r24
+printstatus030:
+	subi	r16, byte1(3600)
+	sbci	r17, byte2(3600)
+	sbci	r18, byte3(3600)
+	sbci	r19, byte4(3600)
+	brmi	printstatus040
+	inc	r24
+	rjmp	printstatus030
+
+printstatus040:
+	sts	pprint+2, r24		; Store HOURs
+	subi	r16, byte1(-3600)	; Compensate surplus subtract
+	sbci	r17, byte2(-3600)
+	sbci	r18, byte3(-3600)
+	sbci	r19, byte4(-3600)
+;
+;	Calculate MINUTEs
+;
+	clr	r24
+printstatus050:
+	subi	r16, byte1(60)
+	sbci	r17, byte2(60)
+	brmi	printstatus060
+	inc	r24
+	rjmp	printstatus050
+
+printstatus060:
+	sts	pprint+3, r24		; Store MINUTESs
+	subi	r16, byte1(-60)		; Compensate surplus subtract
+	sbci	r17, byte2(-60)
+
+	sts	pprint+4, r16		; Store SECONDs
+
+	call	print
+	.db	"SysUpTime ......................:", 0xc0, " Days ", 0xf2, ":", 0xf3, ":", 0xf4, CR, LF, 0
+
 	clc
 	ret
 
